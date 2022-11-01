812a1b72be8446b9a8ce7e63a06933f0998d4ed0
Mohammedsannan <97687050+Mohammedsannan@users.noreply.github.com>
2022-11-01 10:57:51 +0530

    Add files via upload

---
+++ listview/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView list;
+    TextView tv;
+    String items[] = {"football","cricket","batminton","volleyball","hockey"};
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = (ListView) findViewById(R.id.activity_list_view);
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(arrayAdapter);
+
+    }
+
+}
